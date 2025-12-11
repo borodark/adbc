f8d452a13b3c1652eb44b3459bd92e1a6ea954d1
Egor O'Sten <borodark@gmail.com>
2025-12-11 00:48:20 -0500

    pandas worked -> Explorier next

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -15,4 +15,9 @@ windows_exclude =
     _ -> []
   end
 
-ExUnit.start(exclude: pg_exclude ++ windows_exclude)
+# Cube tests are excluded by default since they require a running cubesqld server
+# Run with: mix test --include cube
+# Or use: test/run_cube_tests.sh
+cube_exclude = [:cube]
+
+ExUnit.start(exclude: pg_exclude ++ windows_exclude ++ cube_exclude)

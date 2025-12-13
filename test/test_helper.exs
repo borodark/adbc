2d9fceef736432284e5735d74f670b25d292507c
Egor O'Sten <borodark@gmail.com>
2025-12-12 22:13:35 -0500

    flatbuffers

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -15,9 +15,4 @@ windows_exclude =
     _ -> []
   end
 
-# Cube tests are excluded by default since they require a running cubesqld server
-# Run with: mix test --include cube
-# Or use: test/run_cube_tests.sh
-cube_exclude = [:cube]
-
-ExUnit.start(exclude: pg_exclude ++ windows_exclude ++ cube_exclude)
+ExUnit.start(exclude: pg_exclude ++ windows_exclude)
